187ba108880f49ba3ccb69b8ac781f4e2202484f
Usman Manu <52205926+vmafia@users.noreply.github.com>
2026-06-10 15:54:57 +0700

    Update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import ErrorBoundary from './components/ErrorBoundary.tsx'
+import App from './App'
+import ErrorBoundary from './components/ErrorBoundary'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
